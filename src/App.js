181099d273ede167a15e9771d5912a92df587fcc
Ryan Patterson <ryan@Ryans-MacBook-Air.local>
2017-05-04 20:25:33 -0700

    the goods

--- src/App.js
+++ src/App.js
@@ -28,50 +28,23 @@ function Definition(props) {
 }
 
 class Tile extends Component {
-  propTypes = {
-    color: React.PropTypes.string,
-    title: React.PropTypes.string,
-    quote: React.PropTypes.string,
-    author: React.PropTypes.string,
-    explanation: React.PropTypes.string
-  }
-  state = {
-    front: true
-  }
-
   render() {
     return (<div 
       className={`Tile ${this.props.color}`} 
-      onClick={() => {this.setState({ front: !this.state.front})}}
     >
       <div className="Tile-Content">
           <div className="Tile-Title">
             {this.props.title}
           </div>
-          {this.state.front ? 
-            this.props.children[0]
-          :
-            this.props.children[1]
-          }
+          {this.props.children}
       </div>
     </div>)
   }
 }
 
 /*
-When I think about work, I think a lot about the processes that guide it. 
-I created this list to document some of my thoughts about what guides me.
-It's not meant to be complete or make sense for every situation, but I do
-believe that everything that's listed here is important and resonants with me.
-Processes should follow objectives? I want processes like these?
-
 CSS flip these cards?
 Need more responsive stuff
-
---
-communication and building. looking at objectives. understanding that step backwards are painful ??
-willingness to let go of my own ideas and focus on different aspects ???
-More than just moving fast. Willingness to change direction. That I like building things (and fast).
 */
 
 class App extends Component {
@@ -90,7 +63,12 @@ class App extends Component {
             </Definition>
           </div>
           <Explanation>
-            I made this site because I think it's easier to make decisions when they're based around something, or at least can be measured against something. Often I'll ask myself what I should be doing, or what I want to be doing, or why I'm doing something. And these are tough questions. I don't think I can answer them authoritatively (and I don't think I'd want to). But I do want something I can consider so that when I'm trying to answer these questions, I have something to help guide me. The following are a list of values that I've reflected on and consider important to me and the type of work I want to be doing. This isn't meant to be a complete list of everything that I find valuable, it's just what I came up with when I was thinking about it this time. Maybe if you ask me now (at the time you're reading this), my answer would be different, but for this moment (the one where I'm writing this), these are some ideas that I resonate with very strongly.
+            <p>
+            Often I'll ask myself what I should be doing, or what I want to be doing, or why I'm doing something. And these are tough questions. I don't think I can answer them authoritatively (and I don't think I'd want to). But I do want something I can consider when I'm trying to answer these questions. The following are a list of values that I've reflected on and consider important to me and the type of work I want to be doing.
+            </p>
+            <p>
+            This isn't meant to be a complete list of everything that I find valuable, it's just what I came up with when I was thinking about it. Maybe if you ask me now (at the time you're reading this), my answer would be different, but for this moment (the one where I'm writing this), these are some ideas that I resonate with very strongly.
+            </p>
           </Explanation>
         </Tile>
         <Tile
@@ -101,7 +79,7 @@ class App extends Component {
               Look at all your knowledge as a gift, as a means of helping other people.
           </Quote>
           <Explanation>
-            I think the coolest part about computers is the way they've revolutionized just about everything. I don't think it's always good-- how easy it is to be endlessly distracted, addicted to the endless content, or living in an echo chamber all seem bad to me. But the good it's created has been enormous; things like making completely new and novel experiences, connecting and sharing seemingly infinite amounts of knowledge, the sheer amount of calculations and efficiency its given. I'm in awe of these and very much want to be creating things in this tradition. I think the best thing I can do with technology is help other people, either to do something they could never do before or to do something much better than before. Technology gives us the ability to control more than we could alone and that's very powerful.
+            I think the coolest things about technology is the way that it’s helped people do things more easily. I’m constantly in awe of all the novel experiences technology has created and the way it’s made a seemingly infinite amount of knowledge easier to access. I aspire to use what I know to create things that help people either learn or do things.
           </Explanation>
         </Tile>
         <Tile
@@ -112,8 +90,12 @@ class App extends Component {
               To build may have to be the slow and laborious task of years. To destroy can be the thoughtless act of a single day.
           </Quote>
           <Explanation>
-            There's a double meaning for constructive here. In a literal sense, I want to create things. Building something and seeing the result is rewarding to me. "Things" is a very vague term and I use it consciously because I'm including things like computer programs, creative writing, phyiscal objects, and even my self.
-            But I also think of being constructive in terms of communicating. I think how an idea is conveyed is very important. It is very easy to criticize or fail to understand something. I think it's important to not be dismissive, to listen, to help, to lead others.
+            <p>
+            I mean constructive to have a double meaning. In one sense, I very much want to be creating things. I have a strong internal drive to create and I take a lot of pride in looking back at things that I’ve created. I think this motivation applies to a lot of things, from computer programs and words to physical objects and my self.
+            </p>
+            <p>
+            I also mean constructive in terms of communicating. I think how an idea is conveyed is very important. I don’t want to be fast criticize. I want to try hard to listen and understand where people are coming from and offer ideas in a way that helps things improve.
+            </p>
           </Explanation>
         </Tile>
         <Tile
@@ -124,7 +106,7 @@ class App extends Component {
               Remember that you are more free if you change your opinion and follow those who have corrected your mistakes than if you are stubborn aabout your mistakes.
           </Quote>
           <Explanation>
-            I think it's hard to grow if you're attached to who you are or what you've done. And I very much want to grow. I don't need credit for things. The importance in looking for errors is not to criticize, but to improve. I, as a person, don't want to succeed, but I want what I create to succeed. I want what I create to have merit. To do that, I have to learn. To learn, I have to be open.
+            I think it’s hard for things to improve if they’re too connected to some past idea. I want to have a mindset for growth and I understand that doing that means that I have to let my ego go. I’ve never felt very attached to titles or taking credit for things. I care very much that what I’m doing is good and doing that means taking the best ideas, not my own ideas.
           </Explanation>
         </Tile>
         <Tile
@@ -135,18 +117,18 @@ class App extends Component {
               It is not the place we occupy which is important, but the direction in which we move.
           </Quote>
           <Explanation>
-            I've mentioned that I want to create things, but to be more specific I want to create great things and I think the way to do that is to create a lot of things. I don't think I can do that without being agile. It's also important for being willing to recognize when something isn't right and needs to be changed. I don't think it's good to be stuck on a bad road just because it was decided on long ago. I need to be agile in evaluating where I'm at and where I'm going. Strive for greatness. Doesn't mean go fast at the expense of quality, but to be agile in trying to attain greatness. So it is important to me to constantly strive to create.
+            I've mentioned that I want to create things, but to be more specific I want to create great things and I think the way to do that is to create a lot of things. I don't think I can do that without being agile. I also don’t think of this as a tradeoff with quality, but a tool that can be used to attain it by constantly focusing on what will improve quality the most.
           </Explanation>
         </Tile>
         <Tile
           color="Blue"
           title="Empathy"
         >
-          <Quote author="Oliver Wendell Holmes">
-              It is not the place we occupy which is important, but the direction in which we move.
+          <Quote author="Indian wisdom">
+              Do not be pround, no matter what position you occupy in life. In you and in me and in every other person lives the same God, the same life force; you look down on me in vain; we are all equal beings.
           </Quote>
           <Explanation>
-            I think one of the most valuable things I've ever learned is how to empathize with others. When I think about the greatest movies or books I've read, they've always centered around characters that I can see the world through. Being able to understand others at that level is important to making decisions that will affect others. I want to care. Technology has an impact on others. Let's keep it positive, ummk?
+            I care about other people. It matters to me how they feel. I want to consider that consequences of what I make and how it will impact others. I’m striving to be generous in giving back and trying to understand other people. Technology can be used for good and bad, let’s make it good.
           </Explanation>
         </Tile>
       </div>
